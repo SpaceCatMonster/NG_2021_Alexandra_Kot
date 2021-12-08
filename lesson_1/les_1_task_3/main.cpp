01dde586ec0ada0ce16cddc7ec9bfd5f500c7584
AlexandraKot <fanya0801@gmail.com>
2021-12-08 20:12:17 +0200

    Added lesson 1 task 3

---
+++ lesson_1/les_1_task_3/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a =5;
+    int b =3;
+    cout<<"Before: "<<a<<" "<<b<<endl;
+    swap(a,b);
+    cout<<"After: "<<a<<" "<<b<<endl;
+}
