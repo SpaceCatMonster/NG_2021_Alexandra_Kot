6dae2147bc3a1585f4c57fb7b1b6b357c85d8d90
AlexandraKot <fanya0801@gmail.com>
2022-01-12 19:53:49 +0200

    Less complicated code Lesson 3 Task 3

--- lesson_3/task_3/main.cpp
+++ lesson_3/task_3/main.cpp
@@ -13,20 +13,17 @@ int main()
 
     for(int i = 0; i<4; i++){
         array_1[i] = rand() % 10;
-    }
-
-    for(int i = 0; i<4; i++){
         array_2[i] = rand() % 10;
     }
 
     for(int i = 0; i<4; i++){
-        cout << array_1[i] << " ";
+        cout << array_1[i] << " " <<"+";
+        cout << " "<<array_2[i];
+        cout<<endl;
     }
+
+
     cout<<endl;
-    for(int i = 0; i<4; i++){
-        cout << array_2[i] << " ";
-    }
-    cout<<endl<<endl;
     number_1 = array_1[0]*1000 + array_1[1]*100 + array_1[2]*10 + array_1[3];
     number_2 = array_2[0]*1000 + array_2[1]*100 + array_2[2]*10 + array_2[3];
 
